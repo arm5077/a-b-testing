440a83fe2844e9d7d7876c14ac1a7c61b47489c7
Andrew <arm5077@gmail.com>
2014-12-15 14:35:03 -0500

    Fixes name bug

--- js/app.js
+++ js/app.js
@@ -4,14 +4,22 @@
 	
 	app.controller("FormController", ["$http", function($http){
 		
-		this.sites = [
-			{
-				"url":""
-			}
-		];
+		FormControllerObject = this;
+		this.sites = [{ "url":""}];
+		this.embed_site = false;
 		
 		// Click submit button; send sites to the backend
 		this.submit = function(sites){
+			
+			$http({
+				url: "php/index.php", 
+				method: "GET",
+				params: { content: sites.map(function(item){ return item.url }).join(',')},
+				headers: {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'}
+			}).success(function(data){
+				FormControllerObject.embed_site = data.url;
+				
+			});
 			
 		};
 		
